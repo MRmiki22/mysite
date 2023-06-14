3b8901de6366a0a1635d583dd18d5ca51db4da92
mgeza <mgeza@ZOO>
2023-06-14 20:54:48 +0300

    Album and song tables are added to db

--- music/models.py
+++ music/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Album(models.Model):
+    album_name = models.CharField(max_length=50)
+    album_title = models.CharField(max_length=50)
+    artist_name = models.CharField(max_length=50)
+    album_logo = models.CharField(max_length=1000)
+
+class Song(models.Model):
+    Album = models.ForeignKey(Album, on_delete= models.CASCADE)
