c68b50d891f107602a6ed56505f0bc71b4894a07
mgeza <mgeza@ZOO>
2023-06-15 15:49:48 +0300

    url namespace used

--- music/urls.py
+++ music/urls.py
@@ -30,6 +30,8 @@ Including another URLconf
 
 from django.urls import path,  re_path, include
 from . import views
+
+app_name = 'music'
 urlpatterns = [
     path('', views.index, name = 'index'),
     path('<int:album_id>/', views.detail, name = 'detail')
