79f3c028d3689e1005e61ab6f1b1c310aa2ad606
mgeza <mgeza@ZOO>
2023-06-15 10:31:20 +0300

    in the music models fuction added to return the object string models and also song foreignKey to album table is renamed to album

--- music/models.py
+++ music/models.py
@@ -2,12 +2,17 @@ from django.db import models
 
 # Create your models here.
 class Album(models.Model):
-    album_name = models.CharField(max_length=50)
+    genre = models.CharField(max_length=50)
     album_title = models.CharField(max_length=50)
     artist_name = models.CharField(max_length=50)
     album_logo = models.CharField(max_length=1000)
+    def __str__(self):
+        return self.album_title + ' by ' + self.artist_name
 
 class Song(models.Model):
-    Album = models.ForeignKey(Album, on_delete= models.CASCADE)
+    album = models.ForeignKey(Album, on_delete= models.CASCADE)
     song_title = models.CharField(max_length=50, default= 0)
     file_type = models.CharField(max_length=10, default= 0)
+
+    def __str__(self):
+        return self.song_title + '.' + self.file_type;
