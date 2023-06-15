1169c7d60cb549058200105413deba8550697720
mgeza <mgeza@ZOO>
2023-06-15 12:14:06 +0300

    connecting to DB quiring all albums and list also href details view to every album

--- music/views.py
+++ music/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Album, Song
 # Create your views here.
 
 def index(requist):
-    return HttpResponse("<h2> Album list </h2>")
+    allAlbum = Album.objects.all()
+    html = ''
+    for album in allAlbum:
+        url = '/music/' + str(album.id) + '/'
+        html += ' <a href = "'+url+'">'+ album.album_title +' </a><br>'
+    return HttpResponse(html)
 
 def detail(requist, album_id):
-    return HttpResponse("<h2> Music list </h2>")
+    return HttpResponse("<h2> Music list for album id: "+ str(album_id) + " </h2>")
