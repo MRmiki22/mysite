fb81fa18cd85e078f12044505f6644c6c69b8c39
mgeza <mgeza@ZOO>
2023-06-15 14:27:08 +0300

    raising Http404 for nonexisting album_id in the url

--- music/views.py
+++ music/views.py
@@ -1,17 +1,16 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from django.template import loader
+from django.http import Http404
 from .models import Album, Song
 # Create your views here.
 
 def index(requist):
     allAlbum = Album.objects.all()
-    context = {'allAlbum': allAlbum}
-    return render(requist, 'music/index.html', context)
+    return render(requist, 'music/index.html', {'allAlbum': allAlbum})
 
 def detail(requist, album_id):
+    try:
+        album = Album.objects.get(pk=album_id)
+    except Album.DoesNotExit:
+        raise Http404('Album not found')
     allSong = Song.objects.filter(album = album_id)
-    html = ''
-    for song in allSong:
-        html += '<h3>' + song.song_title+'.' + song.file_type + '</h3><br>'
-    return HttpResponse(html)
+    return render(requist, 'detail.html', {'allSong': allSong, 'allsong': allSong, 'album': album})
