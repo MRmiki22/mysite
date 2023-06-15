f0bed1cb4c42a8f0465d1cbb9b99b1f928098e28
mgeza <mgeza@ZOO>
2023-06-15 17:46:11 +0300

    favorite path added to urlpatterns

--- music/urls.py
+++ music/urls.py
@@ -34,5 +34,6 @@ from . import views
 app_name = 'music'
 urlpatterns = [
     path('', views.index, name = 'index'),
-    path('<int:album_id>/', views.detail, name = 'detail')
+    path('<int:album_id>/', views.detail, name = 'detail'),
+    path('<int:album_id>/fav/', views.fav, name='fav')
 ]
