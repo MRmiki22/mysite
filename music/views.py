89249e948414f2563379fb9d0692758a424333ad
mgeza <mgeza@ZOO>
2023-06-15 17:47:29 +0300

    fav function added to handel favorite form requist

--- music/views.py
+++ music/views.py
@@ -9,3 +9,17 @@ def index(requist):
 def detail(requist, album_id):
     album = get_object_or_404(Album, pk=album_id)
     return render(requist, 'music/detail.html', {'album': album})
+
+def fav(requist, album_id):
+    album = get_object_or_404(Album, pk=album_id)
+    try:
+        selected_song = album.song_set.get(pk = requist.POST['song'])
+    except(keyError, Song.DoesNotExit):
+        return render(requist, 'music/detail.html', {'album': album, 'error_message': "No valid selection"})
+    else:
+        if selected_song.is_fav:
+            selected_song.is_fav = False
+        else:
+            selected_song.is_fav = True
+        selected_song.save()
+        return render(requist, 'music/detail.html', {'album': album})
