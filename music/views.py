339427cf0d0bd0e1b2a90fd279b805ba01550f5a
mgeza <mgeza@ZOO>
2023-06-15 13:45:29 +0300

    index is now templatized

--- music/views.py
+++ music/views.py
@@ -1,15 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 from .models import Album, Song
 # Create your views here.
 
 def index(requist):
     allAlbum = Album.objects.all()
-    html = ''
-    for album in allAlbum:
-        url = '/music/' + str(album.id) + '/'
-        html += ' <a href = "'+url+'">'+ album.album_title +' </a><br>'
-    return HttpResponse(html)
+    template = loader.get_template('music/index.html')
+    context = {'allAlbum' : allAlbum,}
+    return HttpResponse(template.render(context, requist))
 
 def detail(requist, album_id):
-    return HttpResponse("<h2> Music list for album id: "+ str(album_id) + " </h2>")
+    allSong = Song.objects.filter(album = album_id)
+    html = ''
+    for song in allSong:
+        html += '<h3>' + song.song_title+'.' + song.file_type + '</h3><br>'
+    return HttpResponse(html)
