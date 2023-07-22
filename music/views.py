c87cfa13f343a567e22458eb5fc5344a75709db2
mgeza <mgeza@ZOO>
2023-07-22 22:27:39 +0300

    view mathod for favort removed

--- music/views.py
+++ music/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Album, Song
+
 # Create your views here.
 
 def index(requist):
@@ -10,16 +11,3 @@ def detail(requist, album_id):
     album = get_object_or_404(Album, pk=album_id)
     return render(requist, 'music/detail.html', {'album': album})
 
-def fav(requist, album_id):
-    album = get_object_or_404(Album, pk=album_id)
-    try:
-        selected_song = album.song_set.get(pk = requist.POST['song'])
-    except(keyError, Song.DoesNotExit):
-        return render(requist, 'music/detail.html', {'album': album, 'error_message': "No valid selection"})
-    else:
-        if selected_song.is_fav:
-            selected_song.is_fav = False
-        else:
-            selected_song.is_fav = True
-        selected_song.save()
-        return render(requist, 'music/detail.html', {'album': album})
